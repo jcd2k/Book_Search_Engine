a874929ed40bb5c2752592a78f411a7370644084
Joshua Dominguez <joshuadominguez20@gmail.com>
2022-06-19 18:26:40 -0600

    added queries and mutations

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,68 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+      user {
+        _id
+      }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $password: String!, $email: String!) {
+  addUser(username: $username, password: $password, email: $email) {
+    token
+    user {
+      _id
+      username
+      email
+      }
+    }
+  }
+}
+`;
+
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: savedBook!) {
+    saveBook (input: $input)
+        {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                title
+                authors
+                image
+                description
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+    removeBook(bookId:$bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            # _id
+            bookId
+            title
+            authors
+            image
+            description
+            link
+        }
+    }
+}
+`;
